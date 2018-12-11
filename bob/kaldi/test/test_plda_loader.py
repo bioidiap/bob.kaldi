7a3a786c0b2e12c38bcb04f89e22ac7d39352b04
ssarfjoo <saeed.sarfjoo@idiap.ch>
2018-12-11 18:10:02 +0100

    add test for loading plda and vector reading from read_mat function

---
+++ bob/kaldi/test/test_plda_loader.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Seyyed Saeed Sarfjoo <saeed.sarfjoo@idiap.ch>
+# Tue 11 Dec 2018 10:39:15 CET
+
+'''Tests for Kaldi bindings'''
+
+import pkg_resources
+import numpy as np
+import bob.kaldi
+from bob.kaldi.io import read_mat_ark, read_plda
+
+def test_plda():
+    plda_file = pkg_resources.resource_filename(__name__, 'data/plda')
+    plda_dic = read_plda(plda_file)
+    offset_= -1.0 * np.dot(plda_dic['transform'] , plda_dic['mean']).T
+    assert np.sum(offset_) > 0 and np.sum(offset_) < 1
+
+
+def test_readmatrix():
+    ivector_ark = pkg_resources.resource_filename(__name__, 'data/ivector.ark')
+    # read vector ark with read_mat function
+    d = { key:mat for key,mat in read_mat_ark(ivector_ark) }
+    assert len(d) == 281
+
